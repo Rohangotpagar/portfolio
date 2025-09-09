69b254bb7502d5e13db70331cf9bc1c97f8b054e
Rohangotpagar <157702305+Rohangotpagar@users.noreply.github.com>
2025-09-09 14:08:09 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -3,11 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
-  AiFillGithub,
   AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { SiArtstation } from "react-icons/si";  // Import ArtStation icon
 
 function Home2() {
   return (
@@ -19,22 +19,21 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-  I’m a <b className="purple">passionate Game Designer</b> with expertise in 
-  <b className="purple"> Unity</b> and <b className="purple">Unreal Engine</b>, dedicated to creating engaging and immersive gaming experiences 🎮.
-  <br />
-  <br />
-  I specialize in 3D modeling using <b className="purple">Autodesk Maya</b>, as well as crafting detailed textures with 
-  <b className="purple"> Substance Painter</b> to bring environments and characters to life.
-  <br />
-  <br />
-  Beyond game development, I also excel in <b className="purple">UI/UX design</b>, 
-  where I focus on creating intuitive, user-friendly, and visually appealing interfaces that enhance player interaction and overall experience.
-  <br />
-  <br />
-  In addition, I have strong skills in video editing, using <b className="purple">Adobe After Effects</b> and 
-  <b className="purple"> Premiere Pro</b> to deliver polished, high-quality content and cinematic presentations.
-</p>
-
+              I’m a <b className="purple">passionate Game Designer</b> with expertise in 
+              <b className="purple"> Unity</b> and <b className="purple">Unreal Engine</b>, dedicated to creating engaging and immersive gaming experiences 🎮.
+              <br />
+              <br />
+              I specialize in 3D modeling using <b className="purple">Autodesk Maya</b>, as well as crafting detailed textures with 
+              <b className="purple"> Substance Painter</b> to bring environments and characters to life.
+              <br />
+              <br />
+              Beyond game development, I also excel in <b className="purple">UI/UX design</b>, 
+              where I focus on creating intuitive, user-friendly, and visually appealing interfaces that enhance player interaction and overall experience.
+              <br />
+              <br />
+              In addition, I have strong skills in video editing, using <b className="purple">Adobe After Effects</b> and 
+              <b className="purple"> Premiere Pro</b> to deliver polished, high-quality content and cinematic presentations.
+            </p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
@@ -51,12 +50,12 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/Rohangotpagar"
+                  href="https://rohangotpagar.artstation.com/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
-                  <AiFillGithub />
+                  <SiArtstation />
                 </a>
               </li>
               <li className="social-icons">
@@ -64,7 +63,7 @@ function Home2() {
                   href="https://twitter.com/Ronne771"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiOutlineTwitter />
                 </a>
@@ -74,7 +73,7 @@ function Home2() {
                   href="https://www.linkedin.com/in/rohangotpagar/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
