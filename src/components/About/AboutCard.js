ab600460868e5c7e5f9b866dbe3129dd44950193
Rohangotpagar <157702305+Rohangotpagar@users.noreply.github.com>
2025-09-09 15:58:07 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,99 +1,44 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
-import {
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-import { SiArtstation } from "react-icons/si";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
 
-function Home2() {
+function AboutCard() {
   return (
-    <Container fluid className="home-about-section" id="about">
-      <Container>
-        <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
-            <p className="home-about-body">
-              I’m a <b className="purple">passionate Game Designer</b> with expertise in 
-              <b className="purple"> Unity</b> and <b className="purple">Unreal Engine</b>, dedicated to creating engaging and immersive gaming experiences 🎮.
-              <br />
-              <br />
-              I specialize in 3D modeling using <b className="purple">Autodesk Maya</b>, as well as crafting detailed textures with 
-              <b className="purple"> Substance Painter</b> to bring environments and characters to life.
-              <br />
-              <br />
-              Beyond game development, I also excel in <b className="purple">UI/UX design</b>, 
-              where I focus on creating intuitive, user-friendly, and visually appealing interfaces that enhance player interaction and overall experience.
-              <br />
-              <br />
-              In addition, I have strong skills in video editing, using <b className="purple">Adobe After Effects</b> and 
-              <b className="purple"> Premiere Pro</b> to deliver polished, high-quality content and cinematic presentations.
-            </p>
-          </Col>
-          <Col md={4} className="myAvtar">
-            <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://rohangotpagar.artstation.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <SiArtstation />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/Ronne771"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/rohangotpagar/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/rohangotpagar_"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </Container>
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
+            from <span className="purple"> Bhubaneswar, India.</span>
+            <br />
+            I am currently employed as a software developer at Juspay.
+            <br />
+            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
+            Mesra.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
   );
 }
 
-export default Home2;
+export default AboutCard;
