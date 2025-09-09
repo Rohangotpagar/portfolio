43b16a09b4b80dbebdf634a1b61f58b7872e55ae
Rohangotpagar <157702305+Rohangotpagar@users.noreply.github.com>
2025-09-09 15:22:58 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ import Toolstack from "./Toolstack";
 
 function About() {
   return (
-    <Container fluid className="about-section">
+    <Container fluid className="about-section" id="about">
       <Particle />
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
@@ -31,24 +31,7 @@ function About() {
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
-        </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
-
-        <Techstack />
-
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
-        <Toolstack />
-
-        <Github />
-      </Container>
-    </Container>
-  );
-}
-
-export default About;
+            {laptopImg ? (
+              <img src={laptopImg} alt="about" className="img-fluid" />
+            ) : (
+              <p>Image not found</p
